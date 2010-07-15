6b77f1539619a48f82ba0096ef08dad8c1ce93eb
Lally Singh <lally@LS-MBP.local>
2010-07-15 01:19:50 -0400

    Added a parser.

--- mettool.hs
+++ mettool.hs
@@ -1,27 +1,111 @@
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec (sepBy, try, char, eof, many, ParseError, string, (<|>), (<?>), GenParser)
+import Text.ParserCombinators.Parsec.Language
+import Text.ParserCombinators.Parsec.Prim (parse)
+import Text.ParserCombinators.Parsec.Token as P
+import LLVM.Core as LC
 
-data FrameType = fDouble | fFloat | fInt;
+--
+-- Parser types
+-- 
+data FrameType = FDouble 
+               | FFloat 
+               | FInt
+                 deriving Show
 
 data FrameElement = FrameElement FrameType String
+                    deriving Show
+
 data FrameSpecification = FrameSpecification String [FrameElement]
-data EmissionSpec = LangC | LangCpp
+                          deriving Show
+
+data EmissionSpec = LangC 
+                  | LangCpp
+                    deriving Show
 data FullSpecification = Spec EmissionSpec [FrameSpecification]
+                         deriving Show
+
 
 --emitType :: GenParser Char st EmissionSpec
 --
 -- Note: I'm not splitting out the whitespace.  Go a little higher level.
 -- Then,  finally emit my struct decls & the 
-emitC = string "C"
-emitCpp = string "Cpp"
-emitType = string "emit" >> ( emitC <|> emitCpp )
 
-frameSpec = (string "frame") >> 
+lexer :: TokenParser ()
+lexer = makeTokenParser 
+         (javaStyle {
+            reservedNames = [ "emit", "frame", "double", "int", "float" ]
+          , caseSensitive = True
+          } )
 
-frameSpecs = many frameSpec
+whiteSpace= P.whiteSpace lexer
+lexeme    = P.lexeme lexer
+symbol    = P.symbol lexer
+natural   = P.natural lexer
+parens    = P.parens lexer
+semi      = P.semi lexer
+identifier= P.identifier lexer
+reserved  = P.reserved lexer
+reservedOp= P.reservedOp lexer
+                           
 
-commandFile :: GenParser Char st FulLSpecification
+emitCmd = do { try (char 'C' >> char '+' >> char '+'); return LangCpp }
+          <|> do { string "C" >> return LangC }
+          <?> "language name"
+
+ch c = do { Main.whiteSpace; x <- char c; Main.whiteSpace; return x }
+
+emitType = do { Main.reserved "emit" 
+              ; Main.whiteSpace
+              ; lang <- emitCmd
+              ; Main.whiteSpace
+              ; return lang
+              }
+
+elementType = ( Main.reserved "double" >> return FDouble )
+              <|> ( Main.reserved "int" >> return FInt )
+              <|> ( Main.reserved "float" >> return FFloat )
+              <?> "type name"
+
+--element :: Parser FrameElement
+element = do { typ <- elementType
+             ; name <- Main.identifier
+             ; Main.semi
+             ; return (FrameElement typ name)
+             }
+
+--frameSpec :: Parser FrameSpecification
+frameSpec = do { Main.reserved "frame"
+               ; name <- Main.identifier
+               ; ch '{'
+               ; elem <- many element
+               ; ch '}'
+               ; return (FrameSpecification name elem)
+               }
+
+type Parser t = GenParser Char () t
+
+frameSpecs :: Parser [FrameSpecification]
+frameSpecs = do { fr <- many (frameSpec)
+                ; return fr
+                }
+
+
+commandFile :: GenParser Char () FullSpecification
 commandFile =
-            do emitType <- emissionSpec
-               frames <- frameSpecs
-               return (Spec emitType frames)
+            do { emitType <- emitType
+               ; frames <- frameSpecs
+               ; return (Spec emitType frames)
+               }
                
+
+testParse :: Show a => Parser a -> String -> Either ParseError a
+testParse p input = parse (do { Main.whiteSpace
+                            ; x <- p
+                            ; eof
+                            ; return x
+                            }) "input" input
+
+
+main :: IO ()
+main = do { LC.initializeNativeTarget }
+
