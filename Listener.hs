5b56caa5111ae14a9ef17b369b42aad3a6c33827
Lally Singh <lally@acm.org>
2010-12-14 07:31:50 -0500

    Last printf(argv[1]) version.  Now onto the real deal.

--- Listener.hs
+++ Listener.hs
@@ -41,26 +41,72 @@ type MainFunction = Function (Int32 -> Ptr (Ptr Word8) -> IO Int32)
 --
 -- Hmm, I could put these into a tuple type that I take as an argument,
 -- similar to the CPS-style I'll have to do for my structure types.
-type LibCOpenType = Function (Ptr Word8 -> Int32 -> IO Int32)
-type LibCCloseType = Function (Int32 -> IO Int32)
-type LibCWriteType = Function (Int32 -> Ptr Word8 -> Int64 -> IO Int64)
-
+--
+-- We're glancing over the fact that we don't have FILE defined.  Just Ptr Word8
+type FILE = Word8 -- ok, this is easier to read
+type LibCFOpenType = Function (Ptr Word8 -> Ptr Word8 -> IO (Ptr FILE))
+type LibCFCloseType = Function (Ptr FILE -> IO Int32)
+type LibCFWriteType = Function (Ptr Word8 -> Int32 -> Int32 ->Ptr FILE -> IO Int32)
+type LibCSetBufType = Function (Ptr FILE -> Ptr Word8 -> Int32 -> IO ())
+type LibCFPrintf = Function (Ptr Word8 -> Ptr Word8 -> VarArgs Word32)
 buildReaderFun :: String -> CodeGenModule (MainFunction)
 buildReaderFun nm  = do
   printf <- newNamedFunction ExternalLinkage "printf" :: TFunction (Ptr Word8 -> VarArgs Word32)
-  puts <- newNamedFunction ExternalLinkage "puts" :: 
-    TFunction (Ptr Word8 -> IO Word32)
-  open <- newNamedFunction ExternalLinkage "open" :: CodeGenModule(LibCOpenType)
-  close <- newNamedFunction ExternalLinkage "close" :: CodeGenModule(LibCCloseType)
-  write <- newNamedFunction ExternalLinkage "write" :: CodeGenModule(LibCWriteType)
+  puts <- newNamedFunction ExternalLinkage "puts" :: TFunction (Ptr Word8 -> IO Word32)
+  fprintf <- newNamedFunction ExternalLinkage "fprintf" :: CodeGenModule(LibCFPrintf) 
+  fopen <- newNamedFunction ExternalLinkage "fopen" :: CodeGenModule(LibCOpenType)
+  fclose <- newNamedFunction ExternalLinkage "fclose" :: CodeGenModule(LibCCloseType)
+  fwrite <- newNamedFunction ExternalLinkage "fwrite" :: CodeGenModule(LibCWriteType)
+  setbuf <- newNamedFunction ExternalLinkage "setbuf" :: CodeGenModule(LibCSetBufType)
   pattern <- createStringNul "Loading file %s\n" 
   
-  
+  {- program:
+     | Block           | Task                                 |
+     |-----------------+--------------------------------------|
+     | default         | check argc                           |
+     | exit            | exit                                 |
+     | args            | fopen the output                     |
+     |                 | malloc & setbuf it                   |
+     |                 | load shmid, shmsz                    |
+     |                 | attach                               |
+     |                 | init startp, endp, curp, last_seqno  |
+     |                 | init read_cnt = 0                    |
+     |                 | init delay = 100.0 (ms)              |
+     |                 | calc low = size/8, hi = size-size/8  |
+     | loop_head       | load curp, seqno                     |
+     |                 | compare seqno                        |
+     |                 | goto either writebuf,determine_sleep |
+     | writebuf        | fprintf the thing                    |
+     |                 | inc read_cnt                         |
+     |                 | jump back to loop_head               |
+     | determine_sleep | if count < low or count > hi         |
+     |                 | > delay = size/2 * delay / count     |
+     |                 | > delay = max(delay, 100)            |
+     |                 | > delay = min(delay, 2000)           |
+     | do_sleep        | read_cnt = 0                         |
+     |                 | sleep (delay)                        |
+
+    note, a second perror() version may be useful, with a phi'd string
+arg.  as we're always flushing, we can die easily; no need for a
+sighup/sigint handler.  perhaps a third 'nsamples' arg?
+
+
+This proc can be stupid-small, and can be nice'd *far* below the one
+it runs against.
+
+
+   -}
+             
+             
   let callPuts format = (
         createNamedFunction ExternalLinkage "main" $ 
         \ argc argv -> do
           exit <- newBasicBlock
-          cont <- newBasicBlock
+          loop_head <- newBasicBlock
+          writebuf <- newBasicBlock
+          determine_sleep <- newBasicBlock
+          do_sleep <- newBasicBlock
+          args <- newBasicBlock
 
           --
           -- First, verify that we have sufficient arguments
@@ -70,26 +116,18 @@ buildReaderFun nm  = do
           defineBasicBlock exit
           ret (1::Int32)
 
-          defineBasicBlock cont
+          defineBasicBlock args
           p_arg1 <- getElementPtr (argv ::Value (Ptr (Ptr Word8))) (1 :: Int32, ())
           arg1 <- load p_arg1
-          tmp  <- getElementPtr arg1 (0 :: Int32, ()) -- ptr to first char of argv[1]
+          -- tmp = ptr to first char of argv[1]
+          tmp  <- getElementPtr arg1 (0 :: Int32, ()) 
           pattern_p <- getElementPtr (pattern :: Global (Array D16 Word8)) (0 :: Word32, (0 :: Word32, ()))
-
           let printf_s = castVarArgs printf :: Function (Ptr Word8 -> Ptr Word8 -> IO Word32)
           _ <- call  printf_s pattern_p tmp
           ret (0 :: Int32)) :: CodeGenModule (MainFunction)
 
   withStringNul nm callPuts 
   
-     --   tmp <- getElementPtr format (0 :: Word32,(0 :: Word32, ()))
---  return func
---        frameInst <- alloca :: TValue (
---   printf <- newNamedFunction ExternalLinkage "printf" :: TFunction (VarArgs (Ptr Word8))
---   where
---        frameT = determineStruct spec 
-
-
 -- Format spec -> filename 12
 generateReader :: FullSpecification -> String -> IO ()
 generateReader (Spec kid specs) filename = do
