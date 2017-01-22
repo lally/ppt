b54c17139477a0263fb3a64252c054dd5eba7e28
Lally Singh <lally.singh@twosigma.com>
2017-01-22 01:42:12 -0500

    Further IR and Parsing work.

--- src/Ppt/BufferRep.hs
+++ src/Ppt/BufferRep.hs
@@ -1,5 +1,8 @@
-module Ppt.BufferRep where
+{-# LANGUAGE DeriveGeneric #-}
 
+module Ppt.BufferRep where
+import GHC.Generics
+import Data.Aeson
 {- |Parsing and Layout
 
  Ppt builds a buffer specification from its individual Frames.  It
@@ -9,17 +12,17 @@ module Ppt.BufferRep where
 
 
 -- |Common to both Parsed Rep and Machine Layout
-data Primitive = PDouble | PFloat | PInt | PTime | PCounter deriving (Eq, Show)
+data Primitive = PDouble | PFloat | PInt | PTime | PCounter deriving (Generic, Eq, Show)
 
 -- |Used in evaluation.  Some types are promotoed.
 data PrimitiveValue = PVRational Double
                     | PVIntegral Int
                     | PVTime Int
                     | PVCounter [Int]
-                    deriving (Eq, Show)
+                    deriving (Generic, Eq, Show)
 
 -- |Parsed Representation
-data ELanguage = ELangC | ELangCpp deriving (Eq, Show)
+data ELanguage = ELangC | ELangCpp deriving (Generic, Eq, Show)
 
 data ETimeSource = ETimeClockRealtime
                  | ETimeClockRealtimeCoarse
@@ -29,14 +32,16 @@ data ETimeSource = ETimeClockRealtime
                  | ETimeClockBoottime
                  | ETimeClockProcessCputimeId
                  | ETimeClockThreadCputimeId
-                 deriving (Eq, Show)
+                 deriving (Generic, Eq, Show)
 
 -- |gettimeofday() vs clock_gettime()
-data ETimeRep = ETimeVal | ETimeSpec ETimeSource deriving (Eq, Show)
-data ERuntime = ERuntime { erMultithread :: Bool } deriving (Eq, Show)
-data ETag = Tag String String deriving (Eq, Show) -- ^Key, Value
-data EBuffer = EBuffer { ebName :: String, ebMinElements :: Maybe Int } deriving (Eq, Show)
-data EmitOptions = Emit EBuffer ELanguage ETimeRep ERuntime [ETag] deriving (Eq, Show)
+data ETimeRep = ETimeVal | ETimeSpec ETimeSource deriving (Generic, Eq, Show)
+data ERuntime = ERuntime { erMultithread :: Bool } deriving (Generic, Eq, Show)
+data ETag = Tag String String deriving (Generic, Eq, Show) -- ^Key, Value
+data EBuffer = EBuffer { ebName :: String, ebMinElements :: Maybe Int }
+             deriving (Generic, Eq, Show)
+data EmitOptions = Emit EBuffer ELanguage ETimeRep ERuntime [ETag]
+                 deriving (Generic, Eq, Show)
 
 -- |Literal data members of the Frame.  These can be single,
 -- standalone members /or/ a differential.  In the latter case, a
@@ -45,19 +50,71 @@ data EmitOptions = Emit EBuffer ELanguage ETimeRep ERuntime [ETag] deriving (Eq,
 data FrameMember = FMember { fmType :: Primitive
                            , fmName :: String
                            , fmDifferential :: Bool}
-                 deriving (Eq, Show)
+                 deriving (Generic, Eq, Show)
 
 -- |Simple binary operation on two values.  We don't do composite
 -- operations on series, just the built-in ops.
-data FBinOperation = FAdd | FSub | FMul | FDiv deriving (Eq, Show)
+data FBinOperation = FAdd | FSub | FMul | FDiv deriving (Generic, Eq, Show)
+
+{- Operations
+   ----------
+
+   This isn't trivial to figure out.  We have a few types:
+   - A singular stat based on a single frame's data.
+   - A series stat based on all frames' data.
+   - A gap/delta based on differences between frames' data
+
+   So, what's allowable?  There's a high-level type system here.
+   Unerlying it all, I want a single-pass algorithm for calculating
+   these.
+
+   - I can base a stat on just a single frame.
+   - I can base a stat on all frames, creating a series stat.
+   - I can base a stat on applying a single-frame stat on all frames, creating a series stat.
+
+   What requires amore than one pass?
+   - x / var(x) : A series based on a series.  *That's what I can't do.*
+
+   I can do a series based on a singular stat.
+   I can do a singular stat based on a series.
+
+   So, the type system has a primitive value and a series-or-singular
+   indication.  When composing, some trees aren't allowed.  I also
+   have a purely max-of-children value of minimum frames to get a
+   value.
+
+   There are two evaluation scopes:
+   - The current buffer (used by series scope)
+   - The current buffer + series values (used at top level)
+
+   I bucket the evaluation order that way.
+   Example calculations:
+   - (func_time / work) - mean( func_time / work )
+     ^^ current frame     ^^^^^^^^ mean of all frames
+
+   When displaying values, I can just show (insufficient data) on the live display.
+   When saving data, I have two options:
+   - Wait until I have enough data for a full row, and eat data to get there
+   - Save only raw data
+   - Use invalid values for the cells with insufficient data to calculate
+
+   Configuration
+   -------------
+   Use TOML.
+   Accept key-path=value options on a command line flag.
+   Accept alternative configuration files as a flag.
+   Accept ignore-configuration as a flag.
+
+   Accept /new statistics/ on a flag.
+-}
 
 -- |Operation on a series of values.  Should be implementable in accumulateArray
-data FSeriesOperation = FSum | FMean | FVar deriving (Eq, Show)
+data FSeriesOperation = FSum | FMean | FVar deriving (Generic, Eq, Show)
 
 data FrameCalculation = FValue FrameMember
                       | FBinOp FBinOperation FrameCalculation FrameCalculation
                       | FSeriesOp FSeriesOperation FrameCalculation
-                      deriving (Eq, Show)
+                      deriving (Generic, Eq, Show)
 
 -- |Note that we allow calculations to occur on differentials this
 -- way, which is a powerful part of the language.
@@ -73,7 +130,7 @@ data FrameElement = FMemberElem FrameMember
                     -- it should be repored in 'monitor' mode.
                     -- `fcInExport` indicates whether it should be in
                     -- converted output.
-                  deriving (Eq, Show)
+                  deriving (Generic, Eq, Show)
 
 -- |Machine Layout
 data LayoutMember = LMember { lOffset :: Int
@@ -82,16 +139,79 @@ data LayoutMember = LMember { lOffset :: Int
                               -- ^Invisible members are padding or
                               -- synchronization overhead.
                             , lName :: String }
-                   deriving (Eq, Show)
+                   deriving (Generic, Eq, Show)
 
 -- |Statistics are a bit more complex.  A tree of binary opreations is
 -- simple, but it gets a bit more complex on series operations.  We
 -- need a digraph of data dependencies for determining the evaluation
 -- order of the FrameCalculations.
 
+-- It's not a tree.  It's a list that's been created by traversing the
+-- data-dependency tree.  So we need a Plan - a list of evaluations
+-- where they've been resolved, depenency wise.  This also lets me
+-- validate any circular references.
+
+-- Compilation
+--  - Get the expressions for the entire frame at once.
+--  - Use a blackhole system for determining the dependencies (and validating)
+--  - When traversing the graph, build a list of things to evaluate upon exit of traversal from a node.
+--    - Look for something that matches (Eq!) in the list already, and use that.  (CSE built-in)
+--    - Increment a refcount for it.
+--    - Mark reportable ones as such - to indicate which elements
+--      aren't reportable, and can be folded into their dependents.
+
+--  - After the full list, (LATER ON) look for nonreportable things
+--    with refcount 1 to fold into their dependents.
+
+--  - Each node puts out evaluator templates that wrap the accumulateArray slots or the pure function
+--    - We generally want to have one array for all stats.  So each
+--      series element just allocates slots in the array, and then
+--      generates expressions that add into the array.
+ 
+
 --data FrameEvaluator = FEvaluate { feValues :: Map String PrimitiveValue }
 
-data Frame = Frame String [FrameElement] deriving (Eq, Show)
 
+data Frame = Frame String [FrameElement] deriving (Generic, Eq, Show)
 
+-- Look into AESON-ing this thing, to serialize into the generated
+-- source, and to shove into generated output for python
+-- post-processing.
+data Buffer = Buffer EmitOptions [Frame] deriving (Generic, Eq, Show)
+
+instance ToJSON Primitive        
+instance ToJSON PrimitiveValue
+instance ToJSON ELanguage
+instance ToJSON ETimeSource
+instance ToJSON ETimeRep
+instance ToJSON ERuntime
+instance ToJSON ETag
+instance ToJSON EBuffer
+instance ToJSON EmitOptions
+instance ToJSON FrameMember
+instance ToJSON FBinOperation
+instance ToJSON FSeriesOperation
+instance ToJSON FrameCalculation
+instance ToJSON FrameElement
+instance ToJSON LayoutMember
+instance ToJSON Frame
+instance ToJSON Buffer
+
+instance FromJSON Primitive
+instance FromJSON PrimitiveValue
+instance FromJSON ELanguage
+instance FromJSON ETimeSource
+instance FromJSON ETimeRep
+instance FromJSON ERuntime
+instance FromJSON ETag
+instance FromJSON EBuffer
+instance FromJSON EmitOptions
+instance FromJSON FrameMember
+instance FromJSON FBinOperation
+instance FromJSON FSeriesOperation
+instance FromJSON FrameCalculation
+instance FromJSON FrameElement
+instance FromJSON LayoutMember
+instance FromJSON Frame
+instance FromJSON Buffer           
 
--- src/Ppt/Parser.hs
+++ src/Ppt/Parser.hs
@@ -1,17 +1,22 @@
-module Ppt.Parser (parseFile, layoutFrame) where
-import Text.ParserCombinators.Parsec (sepBy1, try, char, eof, many1,
-                                      many, ParseError, digit,
+module Ppt.Parser (parseFile, compileFrame) where
+import Text.ParserCombinators.Parsec (sepBy1, try, char, eof, many1, alphaNum,
+                                      many, ParseError, digit, noneOf,
                                       string, (<|>), (<?>), GenParser)
 import Text.ParserCombinators.Parsec.Language (javaStyle)
 import Text.ParserCombinators.Parsec.Prim (parse)
 import qualified Text.ParserCombinators.Parsec.Token as P
-
+import Data.List
+import Control.Monad
+import Control.Monad.Trans.Either
+import Data.Aeson (encode, decode)
+import Data.ByteString.Lazy.Char8 (pack, unpack)
 import Ppt.BufferRep
 
 lexer :: P.TokenParser ()
 lexer = P.makeTokenParser
          (javaStyle {
-            P.reservedNames = [ "emit", "frame", "double", "int", "float", "time", "default", "buffer" ]
+            P.reservedNames = [ "emit", "frame", "double", "int", "float",
+                                "time", "default", "buffer" ]
           , P.caseSensitive = True
           } )
 
@@ -29,12 +34,50 @@ ch = char
 
 type Parser t = GenParser Char () t
 
-emitCmd :: Parser ELanguage
-emitCmd = do { try (char 'C' >> char '+' >> char '+'); return ELangCpp }
-          <|> do { string "C" >> return ELangC }
+primType = ( resvd "double" >> return PDouble )
+           <|> ( resvd "int" >> return PInt )
+           <|> ( resvd "float" >> return PFloat )
+           <|> ( resvd "time" >> return PTime )
+           <|> ( resvd "counter" >> return PCounter )
+           <?> "type name"
+-- TODO: replace primType here with a higher-level production that
+-- also takes interval and other keywords.  They form a list of
+-- qualified-type-specifiers, which get processed by another function
+-- into the FMemberElem.
+element :: Bool -> Parser [FrameElement]
+element mul = do { typ <- primType
+                 ; names <- identifier `sepBy1` comma
+                 ; semi
+                 ; return (map (\n -> FMemberElem $ FMember typ n mul) names)
+                 }
+
+-- |Primary member parser.  This will need some additions over time.
+-- The syntax will be:
+-- - interval time duration;
+-- - int count;
+-- - [live] [exported] stat field = gap(duration) / count
+frameMember :: Parser [FrameElement]
+frameMember = do { try (resvd "interval" >> element True) }
+              <|> element False
+              <?> "member declaration"
+
+frame = do { resvd "frame"
+           ; name <- identifier
+           ; ch '{'
+           ; ws
+           ; members <- many frameMember
+           ; ws
+           ; ch '}'
+           ; return (Frame name (concat members))
+           }
+
+
+emitCmd :: Parser [ELanguage]
+emitCmd = do { try (char 'C' >> char '+' >> char '+'); return [ELangCpp] }
+          <|> do { string "C" >> return [ELangC] }
           <?> "language name"
 
-emitType :: Parser ELanguage
+emitType :: Parser [ELanguage]
 emitType = do { resvd "emit"
               ; ws
               ; lang <- emitCmd
@@ -48,36 +91,132 @@ bufferValue = ( resvd "default" >> return Nothing )
 bufferCmd = do { resvd "buffer"
                ; name <- identifier
                ; size <- bufferValue
-               ; return (EBuffer name size)
+               ; return [EBuffer name size]
                }
 
-primType = ( resvd "double" >> return PDouble )
-           <|> ( resvd "int" >> return PInt )
-           <|> ( resvd "float" >> return PFloat )
-           <|> ( resvd "time" >> return PTime )
-           <|> ( resvd "counter" >> return PCounter )
-           <?> "type name"
+timeSpecOption :: Parser ETimeSource
+timeSpecOption = (resvd "realtime" >> return ETimeClockRealtime)
+                 <|> (resvd "realtime_coarse" >> return ETimeClockRealtimeCoarse)
+                 <|> (resvd "monotonic" >> return ETimeClockMonotonic)
+                 <|> (resvd "monotonic_course" >> return ETimeClockMonotonicCoarse)
+                 <|> (resvd "monotonic_raw" >> return ETimeClockMonotonicRaw)
+                 <|> (resvd "boottime" >> return ETimeClockBoottime)
+                 <|> (resvd "proc_cputime" >> return ETimeClockProcessCputimeId)
+                 <|> (resvd "thread_cputime" >> return ETimeClockThreadCputimeId)
+                 <?> "clock type"
 
-element :: Bool -> Parser [FrameElement]
-element mul = do { typ <- primType
-                 ; names <- identifier `sepBy1` comma
-                 ; semi
-                 ; return (map (\n -> FMemberElem $ FMember  typ n mul) names)
-                 }
+timeOption ::Parser ETimeRep
+timeOption = (resvd "timeval" >> return ETimeVal)
+             <|> do { resvd "timespec"
+                    ; kind <- timeSpecOption
+                    ; return (ETimeSpec kind)
+                    }
+             <?> "time type"
 
--- |Primary member parser.  This will need some additions over time.
-frameMember :: Parser [FrameElement]
-frameMember = do { try (resvd "interval" >> element True) }
-              <|> element False
-              <?> "member declaration"
+boolOption :: Parser Bool
+boolOption = (resvd "true" >> return True)
+             <|> (resvd "True" >> return True)
+             <|> (resvd "false" >> return False)
+             <|> (resvd "False" >> return False)
+             <?> "bool"
 
-frame = do { resvd "frame"
-           ; name <- identifier
-           ; ch '{'
-           ; members <- many frameMember
-           ; ch '}'
-           ; return (Frame name (concat members))
-           }
+runtimeOption :: Parser ERuntime
+runtimeOption = do { resvd "multithread"
+                   ; val <- boolOption
+                   ; return (ERuntime val) }
 
-parseFile = undefined
-layoutFrame = undefined
+possiblyQuotedString :: Parser String
+possiblyQuotedString = quotedString <|> many (char '_' <|> alphaNum)
+
+quotedString = do
+    char '"'
+    x <- many (noneOf "\"" <|> (char '\\' >> char '\"'))
+    char '"'
+    return x
+
+tagOption = do { resvd "tag"
+               ; key <- possiblyQuotedString
+               ; val <- possiblyQuotedString
+               ; return (Tag key val) }
+
+data PartialOption = OptLang ELanguage
+                   | OptBuffer EBuffer
+                   | OptTime ETimeRep
+                   | OptRuntime ERuntime
+                   | OptTags [ETag]
+                   deriving (Eq, Show)
+
+optionParser :: Parser [PartialOption]
+optionParser = (do { resvd "time"
+                   ; timeOpt <- timeOption
+                   ; return [OptTime timeOpt] })
+               <|> (do { resvd "runtime"
+                       ; run <- runtimeOption
+                       ; return [OptRuntime run] })
+               <|> (do { tag <- tagOption
+                       ; return [OptTags [tag]] })
+               <?> "option type: time, runtime, tag"
+
+optionSplitter :: PartialOption -> ([ELanguage], [EBuffer], [ETimeRep], [ERuntime], [[ETag]])
+optionSplitter (OptLang l)    = ([l], [], [], [], [])
+optionSplitter (OptBuffer b)  = ([], [b], [], [], [])
+optionSplitter (OptTime t)    = ([], [], [t], [], [])
+optionSplitter (OptRuntime r) = ([], [], [], [r], [])
+optionSplitter (OptTags ts)   = ([], [], [], [], [ts])
+
+defaultEmit = Emit (EBuffer "unlisted" Nothing) ELangCpp (ETimeSpec ETimeClockMonotonic) (ERuntime False) []
+
+optionCombine :: [PartialOption] -> Either String EmitOptions
+optionCombine opts = let
+  addUp (a, b, c, d, e) (f, g, h, i, j) = (a ++ f, b ++ g, c ++ h, d ++ i, e ++ j)
+  (langs, buffers, timereps, runtimes, rawTags) = foldl' addUp ([], [], [], [], []) (
+    map optionSplitter opts)
+  tags = concat rawTags
+  min1 :: [a] -> String -> Either String a
+  min1 (x:_)  _ = Right x
+  min1 [] name = Left (name ++ " required")
+  max1 :: [a] -> String -> a -> Either String a
+  max1 (x:[]) _ _ = Right x
+  max1 [] _ def = Right def
+  max1 _ name def = Left ("Only 1 " ++ name ++ " allowed")
+  exact1 :: [a] -> String -> Either String a
+  exact1 (x:[]) _  = Right x
+  exact1 _ name = Left ("Exactly 1 " ++ name ++ " required")
+  in (Emit <$> max1 buffers "buffer line" (EBuffer "unlisted" Nothing)
+       <*> exact1 langs "Language"
+       <*> max1 timereps "time representation" (ETimeSpec ETimeClockMonotonic)
+       <*> max1 runtimes "multithread option" (ERuntime False)
+       <*> (pure tags))
+
+headParser :: Parser EmitOptions
+headParser = do { options <- many ((resvd "option" >> optionParser)
+                                   <|> (do { r <- emitCmd ; return (map OptLang r)})
+                                   <|> (do { r <- bufferCmd; return (map OptBuffer r)})
+                                   <?> "header matter")
+                ; case (optionCombine $ concat options) of
+                    Left msg -> fail msg
+                    Right opts -> return opts
+                }
+
+
+parseText :: Show a => Parser a -> String -> String -> Either ParseError a
+parseText p input fname = parse (do { ws
+                                    ; x <- p
+                                    ; eof
+                                    ; return x
+                                    }) fname input
+parseFile :: String -> String -> Either ParseError Buffer
+parseFile input fname = parse (do { ws
+                                  ; head <- headParser
+                                  ; frames <- many frame
+                                  ; return (Buffer head frames) }) fname input
+
+readBuffer :: String -> Maybe Buffer
+readBuffer s = decode (pack s)
+
+saveBuffer :: Buffer -> String
+saveBuffer b = unpack $ encode b
+
+
+compileFrame = undefined
+
