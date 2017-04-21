957b6f3a60c698693ae7f28c8a57446e19893929
Lally Singh <lally.singh@twosigma.com>
2017-04-21 10:45:32 -0400

    Output file format seems reasonable from eyeball inspection.

--- src/Ppt/Agent/Command.hs
+++ src/Ppt/Agent/Command.hs
@@ -25,6 +25,7 @@ import Ppt.Frame.Layout             hiding (sizeOf)
 import Prelude                      hiding (length)
 import System.IO
 import Numeric
+import qualified Data.Binary as B
 import qualified Data.List as L
 import qualified Data.Vector.Storable         as V
 import qualified Data.Vector.Storable.Mutable as VM
@@ -45,13 +46,22 @@ makeLenses ''Flag
 
 
 arglist :: [GO.OptDescr Flag]
-arglist = [GO.Option ['p'] ["pid"] (GO.ReqArg (\t -> Pid (read t)) "pid") "Pid to attach to."
-          , GO.Option ['b'] ["buffer"] (GO.ReqArg (\t -> BufferName t) "buffer name") "Name of buffer to read."
-          , GO.Option ['l'] ["list-buffers"] (GO.NoArg (ListBuffers 0)) "List buffers in process."
-          , GO.Option ['h', '?'] ["help"] (GO.NoArg (ShowHelp 0)) "Print help."
-          , GO.Option ['d'] ["desc"] (GO.ReqArg (\t -> Desc t) "description") "Comment on the collection session."
-          , GO.Option ['D'] ["descfile"] (GO.ReqArg (\t -> DescFile t) "descriptionfile") "File containing comment on the collection session."
-          , GO.Option ['t'] ["time-offset"] (GO.ReqArg (\t -> TimeOffset (read t)) "delta-hours") "Apply this change to the timestamps on this machine." ]
+arglist = [GO.Option ['p'] ["pid"] (GO.ReqArg (\t -> Pid (read t)) "pid")
+            "Pid to attach to. (required)"
+          , GO.Option ['b'] ["buffer"] (GO.ReqArg (\t -> BufferName t) "buffer name")
+            "Name of buffer to read.  (required)"
+          , GO.Option ['l'] ["list-buffers"] (GO.NoArg (ListBuffers 0))
+            "List buffers in process."
+          , GO.Option ['h', '?'] ["help"] (GO.NoArg (ShowHelp 0))
+            "Print help."
+          , GO.Option ['d'] ["desc"] (GO.ReqArg (\t -> Desc t) "description")
+            "A comment on the collection session."
+          , GO.Option ['D'] ["descfile"] (GO.ReqArg (\t -> DescFile t) "descriptionfile")
+            "File containing comment on the collection session."
+          , GO.Option ['t'] ["time-offset"] (GO.ReqArg (\t -> TimeOffset (read t)) "delta-hours")
+            "Apply this change to the timestamps on this machine."
+          , GO.Option ['o'] ["output-file"] (GO.ReqArg (\t -> Filename t) "output-file")
+            "Output file to save buffer contents. (required)"]
 
 data Execution = Exec { ePid        :: Int
                       , eFilename   :: String
@@ -94,12 +104,23 @@ combine desc flags =
            let rest = tail s
            put rest
            return $ Just (read $ head s)
+     takeDefault :: Read a => Maybe a -> a -> State [String] (a)
+     takeDefault (Just s) def = return s
+     takeDefault Nothing def = do
+       s <- get
+       case s of
+         [] -> return def
+         _ -> do
+           let rest = tail s
+           put rest
+           return (read $ head s)
+
      (result, _) = flip runState remainders $ do
        _pid <- tryTake pId
-       _filename <- tryTake filename
-       _timeOff <- tryTake timeoffset
        _bufname <- tryTake bufname
-       return $ Exec <$> _pid <*> _filename <*> (pure desc) <*> _timeOff <*> _bufname
+       _filename <- tryTake filename
+       _timeOff <- takeDefault timeoffset 0.0
+       return $ Exec <$> _pid <*> _filename <*> (pure desc) <*> (pure _timeOff) <*> _bufname
  in result
 
 parseArgs :: [String] -> IO (Execution)
@@ -131,19 +152,21 @@ saveFile name header = do
       textLen = (fromIntegral $ length text) :: Int32
       magic = 0x50505431 :: Int32
   h <- openBinaryFile name WriteMode
-  hSetBuffering h (BlockBuffering $ Just 1048576)
-  hPut h (toByteString magic)
-  hPut h (toByteString textLen)
+  hSetBuffering h (BlockBuffering $ Just 8)
+  hPut h (B.encode  magic)
+  hPut h (B.encode textLen)
   hPut h text
+  hFlush h
   return h
 
 writeBufferToFile :: Storable a => Handle -> VM.IOVector a -> Int -> Int -> IO ()
 writeBufferToFile h vec startIndex nelements = do
   VM.unsafeWith vec $ \p -> do
     -- Don't actually need the data, just a value of 'a' from Ptr a to get its size.
-    let start = plusPtr p startIndex
     first <- peek p
     let size = sizeOf first
+        start = plusPtr p (startIndex * size)
+    putStrLn $ "calling hPutBuf with p=" ++ (show p) ++ " start=" ++ (show start) ++ "  size=" ++ (show $ size * nelements) ++ " and startIndex=" ++ show startIndex
     hPutBuf h start (size * nelements)
 
 -- Actual command implementations of Agent-mode commands (attach)
@@ -156,10 +179,11 @@ processBufferValues desc timeOffset fileName shmPtr json bufElems = do
       execLoop file cursor seqno saveBuffer = do
             (count, cursor', seqno') <- readBuffer destP cursor seqno bufElems elemSizeInWords saveBuffer
             putStrLn $ ">> Count: " ++ show count ++ ", Cursor: " ++ show cursor' ++ ", seqno: " ++ show seqno'
+            putStrLn $ ">> bufElems: " ++ show bufElems
             putStrLn $ ">> shmPtr: " ++ (showHex shmPtr ", destIntPtr: ") ++ (showHex  destIntPtr "")
             putStrLn $ ">> that's an offset of " ++ show (destIntPtr - fromIntegral shmPtr)
             putStrLn $ ">> Size of an object: " ++ (show elemSize) ++ ", " ++ (show elemSizeInWords) ++ " words."
-            if (bufElems < cursor + count)
+            if ((cursor + count) < bufElems)
               then writeBufferToFile file saveBuffer cursor count
               else do
                 let endPart = bufElems - cursor
@@ -167,6 +191,7 @@ processBufferValues desc timeOffset fileName shmPtr json bufElems = do
                 writeBufferToFile file saveBuffer cursor endPart
                 writeBufferToFile file saveBuffer 0 firstPart
             -- TODO: make this dynamic timing based on count vs bufsize.
+            hFlush file -- TODO: only do this occasionally.
             threadDelay 250000
             execLoop file cursor' seqno' saveBuffer
   file <- saveFile fileName (FileRecord "1.0.0" "now" desc (round $ timeOffset * 3600.0) json)
@@ -178,6 +203,7 @@ processBufferValues desc timeOffset fileName shmPtr json bufElems = do
 attach :: [String] -> IO ()
 attach args = do
   command <- parseArgs args
+  putStrLn $ "got args: " ++ show args
   case command of
     Exec pid fname desc toff bufname ->
       attachAndRun pid bufname (processBufferValues desc toff fname)
